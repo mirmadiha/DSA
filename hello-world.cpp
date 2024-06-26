7e2a2efc83c3ac4170bb9a8711bd5503b05fa137
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-06-26 22:49:16 +0530

    updated

--- hello-world.cpp
+++ hello-world.cpp
@@ -2,4 +2,27 @@
 using namespace std;
 int main(){
     cout<<"Hello World:-)"<<endl;
+
+    //Datatypes
+    char b='v';
+    cout<<b<<endl;
+
+    bool c=true;
+    cout<<c<<endl;
+
+    float d=2.5;
+    cout<<d<<endl;
+
+    double e=2.666;
+    cout<<e<<endl;
+
+    int f=sizeof(b);
+    cout<<"size of char is:"<<f<<endl;
+
+    //Typecasting
+    int a='a';
+    cout<<a<<endl;
+
+    char ch=98;
+    cout<<ch<<endl;
 }
