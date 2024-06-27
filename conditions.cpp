e5ed04680fee2648dbd700aa140a441e3505c831
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-06-27 23:31:23 +0530

    conditions

---
+++ conditions.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    if(a>3){
+        cout<<"true"<<endl;
+    }
+    else{
+        cout<<"false"<<endl;
+    }
+    int b,c;
+    cin>>a>>b;
+    cout<<"value of a and b is : "<<a<<" "<<b<<endl;
+}
