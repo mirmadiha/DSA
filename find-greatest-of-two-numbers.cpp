10c2927937ebdf1a28b0f369774154acb66747d5
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-06-27 23:48:28 +0530

    conditions

---
+++ find-greatest-of-two-numbers.cpp
@@ -0,0 +1,17 @@
+/*Write a program in C++ to find the greatest of two numbers*/
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+cout<<"Enter the first number = "<<endl;
+cin>>a;
+cout<<"Enter the second number = "<<endl;
+cin>>b;
+if(a>b){
+    cout<<"A is the greatest number"<<endl;
+}
+else{
+    cout<<"B is the greatest number"<<endl;
+}
+}
