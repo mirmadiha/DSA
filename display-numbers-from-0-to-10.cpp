c63ab4dbab2982eebc263bad7cec47a2a8ebc5c8
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-06-28 13:47:07 +0530

    while loop

---
+++ display-numbers-from-0-to-10.cpp
@@ -0,0 +1,13 @@
+/*Write a program in C++ to display numbers from 0 to 10 using while loop*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number"<<" ";
+    cin>>n;
+        int i=0;
+        while(i<=n){
+            cout<<i<<endl;
+            i++;
+        }
+}   
