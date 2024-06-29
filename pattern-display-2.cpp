4fc1fa00fe7dd589a63260e9722671ce8744794f
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-06-29 22:50:19 +0530

    pattern

---
+++ pattern-display-2.cpp
@@ -0,0 +1,23 @@
+/*Write a program in C++ to display the pattern:
+1 1 1
+2 2 2 
+3 3 3 
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number of rows:";
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<i;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
