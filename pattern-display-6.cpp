297d95f64d4b875827ad0fcfa73924efb2e54753
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-06-30 18:46:30 +0530

    pattern

---
+++ pattern-display-6.cpp
@@ -0,0 +1,24 @@
+/*Write a program in C++ to display the following pattern
+* 
+* *
+* * *
+* * * *
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number of rows:"<<" ";
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<"*"<<" ";
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
