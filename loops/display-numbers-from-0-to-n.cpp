ac0acc324015a7d5d2a37aa4c8db695cccfc98cd
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-07-03 17:40:14 +0530

    display number

---
+++ loops/display-numbers-from-0-to-n.cpp
@@ -0,0 +1,12 @@
+/*Write a program in C++ to display numbers from 0 to n using for loop*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number:"<<" ";
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        cout<<endl<<i<<endl;
+    }
+}
