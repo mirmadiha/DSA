4c9b39098017d8c212ba2cfec36f5583bbf91bbe
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-10-19 11:38:21 +0530

    2^n

---
+++ recursion/power-of-n.cpp
@@ -0,0 +1,18 @@
+/*Write a program in C++ to */
+#include<iostream>
+using namespace std;
+int power(int n){
+    if(n==0){
+        return 1;
+    }
+    int result=2*power(n-1);
+    return result;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the value of n"<<" ";
+    cin>>n;
+    int ans=power(n);
+    cout<<ans<<endl;
+}
