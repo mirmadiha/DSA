224a3a494f6a7bb7c25b2006cd93d25647a535e2
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2026-02-04 23:36:26 +0530

    displaying head data
    Implement function to convert an array to a linked list.

---
+++ Linked List/convert-array-to-lnkedlist.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1,Node* node1){
+        data=data1;
+        next=node1;
+    }
+
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+Node* convertArrayToLinkedList(vector<int>&arr){
+    Node* head=new Node(arr[0]);
+    Node* prev=head;
+    for(int i=1;i<arr.size();i++){
+        Node* curr=new Node(arr[i]);
+        prev->next=curr;
+        prev=curr;
+    }
+    return head;
+}
+
+int main () {
+    vector<int>arr={2,6,4,64,5,3};
+    Node* result=convertArrayToLinkedList(arr);
+    cout<<result->data<<endl;
+}
